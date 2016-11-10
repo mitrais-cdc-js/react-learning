bc5c522f4471f1cde2e9daa61ad660451727c9c3
andry-baka <andry.baka@mitrais.com>
2016-11-10 14:35:34 +0800

    Add Knowledge Sharing 05 Redux Step by Step

---
+++ Knowledge Sharing/05 Redux Step by Step/app/bankReducer.js
@@ -0,0 +1,26 @@
+import constants from './constants';
+
+const initialState = {
+  balance: 0
+}
+
+const bankReducer = (state, action) => {
+  console.log('State: ' + JSON.stringify(state));   //temporary logging all states
+  console.log('Action: ' + JSON.stringify(action)); //temporary logging all actions
+
+  switch (action.type) {
+    case constants.CREATE_ACCOUNT:
+      return initialState;
+
+    case constants.DEPOSIT_INTO_ACCOUNT:
+      return {balance: state.balance + parseFloat(action.amount)};
+
+    case constants.WITHDRAW_FROM_ACCOUNT:
+      return {balance: state.balance - parseFloat(action.amount)};
+
+    default:
+      return state;
+  }
+};
+
+export default bankReducer;
---
+++ Knowledge Sharing/05 Redux Step by Step/app/constants.js
@@ -0,0 +1,5 @@
+export default {
+  CREATE_ACCOUNT: 'create account',
+  WITHDRAW_FROM_ACCOUNT: 'withdraw from account',
+  DEPOSIT_INTO_ACCOUNT: 'deposit into account'
+};
